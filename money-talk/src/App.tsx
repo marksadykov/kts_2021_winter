417d3b6ebe4ac07e72df27e95620fbbfe590e4a3
marksadykov <sadykov.mark@gmail.com>
2021-02-04 22:19:53 +0000

    route

--- money-talk/src/App.tsx
+++ money-talk/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {
+    AppRoot,
+    View,
+    Panel,
+    PanelHeader,
+    Header,
+    Group,
+    Cell,
+    CardScroll,
+    Card,
+    Root,
+    CellButton
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+import Home from "./components/Home/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+
+    return (
+        <Home />
+    );
 }
 
 export default App;
---
+++ money-talk/src/components/Home/Home.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import {Card, CardScroll, CellButton, Group, Panel, PanelHeader, Root, View} from "@vkontakte/vkui";
+import {useState} from "react";
+
+const Home = () => {
+
+    const [activeView, setActiveView] = useState('home');
+
+    return (
+            <Root activeView={activeView}>
+                <View activePanel="card" id="home">
+                    <Panel id="card">
+                        <PanelHeader>CardScroll</PanelHeader>
+                        <Group description="Внутри Group">
+                            <CardScroll size="s">
+                                <Card>
+                                    <div style={{ paddingBottom: '66%' }}>
+                                        <CellButton onClick={ () => setActiveView('kek') }>
+                                            kek
+                                        </CellButton>
+                                    </div>
+                                </Card>
+                                <Card>
+                                    <div style={{ paddingBottom: '66%' }}>
+                                        <CellButton onClick={ () => setActiveView('lol') }>
+                                            lol
+                                        </CellButton>
+                                    </div>
+                                </Card>
+                                <Card>
+                                    <div style={{ paddingBottom: '66%' }} />
+                                </Card>
+                                <Card>
+                                    <div style={{ paddingBottom: '66%' }} />
+                                </Card>
+                                <Card>
+                                    <div style={{ paddingBottom: '66%' }} />
+                                </Card>
+                                <Card>
+                                    <div style={{ paddingBottom: '66%' }} />
+                                </Card>
+                            </CardScroll>
+                        </Group>
+                        <CardScroll size="m">
+                            <Card>
+                                <div style={{ paddingBottom: '42%' }} />
+                            </Card>
+                            <Card>
+                                <div style={{ paddingBottom: '42%' }} />
+                            </Card>
+                            <Card>
+                                <div style={{ paddingBottom: '42%' }} />
+                            </Card>
+                            <Card>
+                                <div style={{ paddingBottom: '42%' }} />
+                            </Card>
+                        </CardScroll>
+                        <CardScroll size="l">
+                            <Card>
+                                <div style={{ paddingBottom: '29%' }} />
+                            </Card>
+                            <Card>
+                                <div style={{ paddingBottom: '29%' }} />
+                            </Card>
+                            <Card>
+                                <div style={{ paddingBottom: '29%' }} />
+                            </Card>
+                        </CardScroll>
+                    </Panel>
+                </View>
+                <View activePanel="card" id="kek">
+                    <Panel id="card">
+                        <PanelHeader>CardScroll</PanelHeader>
+                    </Panel>
+                </View>
+                <View activePanel="card" id="lol">
+                    <Panel id="card">
+                        <PanelHeader>lol</PanelHeader>
+                    </Panel>
+                </View>
+            </Root>);
+
+};
+
+export default Home;

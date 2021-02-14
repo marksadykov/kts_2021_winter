ac9ab5255ac8eaf18a9dacae023a9b459a24f1a1
marksadykov <sadykov.mark@gmail.com>
2021-02-14 16:21:11 +0000

    ghaph

--- money-talk/src/components/Chart/utils/makeInterval.tsx
+++ money-talk/src/components/Chart/utils/makeInterval.tsx
@@ -1,30 +1,18 @@
 import * as React from 'react';
 
+const changeHours = (currentDate: Date, hours: number) => {
+    const date = new Date(currentDate.valueOf());
+    date.setDate(date.getHours() - hours);
+    return date;
+}
+
 const makeInterval = () => {
-    const today = new Date();
-    const prev = today.setDate(today.getDate() - 4);
-    const currentToday = {
-      year: today.getFullYear(),
-      month: today.getMonth() + 1,
-      day: today.getDate(),
-      hours: today.getHours(),
-    };
-    // const currentPrev = {
-    //     year: prev.getFullYear(),
-    //     month: prev.getMonth() + 1,
-    //     day: prev.getDate(),
-    //     hours: prev.getHours(),
-    // };
+    const end = new Date();
 
-    console.log(prev);
+    const start = changeHours(end, 4);
 
-    const endZeroMonth = (currentToday.month < 10) ? '0' : '';
-    const startZeroMonth = (currentToday.month < 10) ? '0' : '';
-
-    const end = `${currentToday.year}-${endZeroMonth}${currentToday.month}-${currentToday.day}T0${currentToday.hours}%3A00%3A00Z`;
-    const start = `${currentToday.year}-0${currentToday.month}-${currentToday.day}T0${currentToday.hours-4}%3A00%3A00Z`;
-
-    return [start, end];
+    console.log(start, end);
+    return [start.toISOString(), end.toISOString()];
 }
 
 export default makeInterval;

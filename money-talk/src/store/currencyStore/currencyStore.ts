2df890b3ee33dc4351f0b367b303004f08d33f93
marksadykov <sadykov.mark@gmail.com>
2021-02-22 02:35:30 +0300

    api

--- money-talk/src/store/currencyStore/currencyStore.ts
+++ money-talk/src/store/currencyStore/currencyStore.ts
@@ -1,8 +1,14 @@
 import { observable, action, makeObservable } from 'mobx';
+import { currencyModel } from '../models';
 
 export default class currencyStore {
+    _currencyFirst: currencyModel;
+    _currencySecond: currencyModel;
 
     constructor() {
-
+        makeObservable(this, {
+            _currencyFirst: observable,
+            _currencySecond: observable,
+        });
     }
 }
